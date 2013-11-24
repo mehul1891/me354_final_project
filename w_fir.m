949292bba2f93f49fb9874b4637d038469b42a2b
Mehul Oswal <moswal@stanford.edu>
2013-11-24 02:45:19 -0800

    added a new file

---
+++ w_fir.m
@@ -0,0 +1,17 @@
+%=========================================================================%
+
+% IMAGE FILTERING CODE: 1.0 
+
+% Developers             : Mehul Oswal
+% Organization           : Stanford University
+% Objective              : Perform basic image filtering
+% Contact information    : moswal@stanford.edu
+
+% Input options
+
+
+
+
+
+%=========================================================================%
+
