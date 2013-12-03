c1154c6bb53c981a439c3808c68acc0d7f03c9d4
Mehul Oswal <moswal@stanford.edu>
2013-12-03 15:50:58 -0800

    Made some changes. Corrected one implementation. Ran numerical tests to ascertain some optimal variable choices.

--- w_fir.m
+++ w_fir.m
@@ -23,6 +23,9 @@ clear all;
 close all;
 clc;
 
+disp('Extracting image and initializing variables')
+tic;
+
 % Grab the BLURRED image from the respective directory
 im = imread...
     ('/Users/mehuloswal/me354_final_project/image_cereals/image2.jpg');
@@ -39,25 +42,30 @@ im = im2double(im);
 % Image size
 [m,n] = size(im);
 
-% Window size (M) [Later make this as an input]
-M = 3;
+% Window size (M)                       [Later make this as an input]
+M = 4;
 W = -M:M;
-
-% St.D. of noise                        [Later make this as an input]
-sig_n = 0.1;
 
 % St.D of the image
 sig = std2(im);
+
+% St.D. of noise                        [Later make this as an input]
+% sig_n = 0.01;
+sig_n = 0.08*sig;
 
 % Signal to Noise ratio (SNR)
 SNR = sig.^2/sig_n.^2;
 
 % Covariance function (r): size = [2p+1,2p+1]
-p = 1;                               % Need to check/play on this parameter
+p = 3;                               % Need to check/play on this parameter
 mpr = -p:p; npr = -p:p;
 
-%% Evaluating the RIGHT HAND SIDE OF Eqn. 8.64
+disp('Done')
+toc;
 
+%% Evaluating the RIGHT HAND SIDE OF Eqn. 8.64
+disp('Evaluating the RIGHT HAND SIDE OF Eqn. 8.64')
+tic;
 % Creating h (PSF) AND r0 (Covariance)
 r0 = zeros(length(mpr),length(npr));
 h  = zeros(length(mpr),length(npr));
@@ -68,10 +76,13 @@ for i = -p:p
 
         % PSF (h) : size = [2p+1,2p+1] 
         % Gaussian                      [Later make this as an input]
-        alpha = 0.01; % This should be made smaller
-        h(i+p+1,j+p+1) = exp(-alpha*(i^2 + j^2));
+%         alpha = .01; % This should be made smaller
+%         h(i+p+1,j+p+1) = exp(-alpha*(i^2 + j^2));
     end
 end
+
+% To use MATLAB's inbuilt function to create the PSF
+h = fspecial('disk',p);
 
 % r_uv = Convolution between PSF and r0
 r_uv = zeros(2*M+1,2*M+1);              % Initializing
@@ -93,11 +104,16 @@ for k = 1:length(W)
     end
 end
 
-% Reorganizing r_uv from a matrix to a vector
+% Reshaping matrix r_uv into a vector
 r_uv = reshape(r_uv,(2*M+1)^2,1);
+
+disp('Done')
+toc;
 
 %% Evaluating the LEFT HAND SIDE OF Eqn. 8.64
 
+disp('Evaluating the LEFT HAND SIDE OF Eqn. 8.64')
+tic;
 R = zeros((2*M+1)^2,(2*M+1)^2);
 I = eye(size(R,1),size(R,2));
 
@@ -141,24 +157,39 @@ for k = 1:length(W)
     end
 end
 
+%===========================BEGIN-NEW=====================================%
+delta = eye(size(R_basic,1),size(R_basic,2));
+LHS_basic = (1/SNR).*delta + R_basic;
+row = [LHS_basic(1,:),zeros(1,(2*M+1)^2-length(LHS_basic(1,:)))];
+col = [LHS_basic(:,1);zeros((2*M+1)^2-length(LHS_basic(1,:)),1)];
 
-% Now to create a block Toeplitz matrix R
-row = [R_basic(1,:),zeros(1,(2*M+1)^2-length(R_basic(1,:)))];
-col = [R_basic(:,1);zeros((2*M+1)^2-length(R_basic(1,:)),1)];
+LHS = toeplitz(col,row);
+%===========================END-NEW=======================================%
 
-R = toeplitz(col,row);
-
-% Lets call matrix LHS : size((2*M+1)^2,(2*M+1)^2))
-LHS = (1/SNR).*I + R;
+%===========================BEGIN-OLD=======================================%
+% % Now to create a block Toeplitz matrix R
+% row = [R_basic(1,:),zeros(1,(2*M+1)^2-length(R_basic(1,:)))];
+% col = [R_basic(:,1);zeros((2*M+1)^2-length(R_basic(1,:)),1)];
+% 
+% R = toeplitz(col,row);
+% 
+% % Lets call matrix LHS : size((2*M+1)^2,(2*M+1)^2))
+% LHS = (1/SNR).*I + R;
+%===========================END-OLD=======================================%
 
 % To get g, we need to solve the system of equations LHS*g = r_uv
-
 g = LHS\r_uv;
 
+% reshaping g back to a matrix
 g = reshape(g,2*M+1,2*M+1);
 
+disp('Done')
+toc;
 
 %% DFFT of the image
+
+disp('Convolving blurred image with FIR Wiener Filter')
+tic;
 im_sharp = zeros(size(im));
 % Convolution
 for k = 1:m
@@ -176,4 +207,16 @@ for k = 1:m
         end        
     end
 end
+
+disp('Done')
+toc;
+
+%% Sharpedned image
+
+figure(1)
+imshow(im)
+title('original image')
+
+figure(2)
 imshow(im_sharp)
+title('Sharpened Image')
